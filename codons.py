93c64b0dc6f2d9716cd4997604e255e89ffcf60f
maayan-wag <maayan.wagshal@gmail.com>
2025-12-27 18:24:26 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,14 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+    file = open(file_path, "r")
+    rows = file.readlines()
+    file.close()
+    codon_mapping = {}
+    for row in rows:
+        cells = row.strip().split('\t')
+        if len(cells) >= 3: # Basic safety check to avoid empty lines
+            codon = cells[0]
+            amino_acid = cells[2]
+            codon_mapping[codon] = amino_acid
+    return codon_mapping
 
 
